2177c96d94a997c2d75571868ba4d2f3e8f6628a
Sven Eschlbeck <66701689+sveneschlbeck@users.noreply.github.com>
2021-12-22 00:23:52 +0100

    Update module_template.py

--- templates/code_templates/module_template.py
+++ templates/code_templates/module_template.py
@@ -1,11 +1,30 @@
 """
-============
-Module Title
-============
-Simply and short description of the module's purpose.
+==========
+Person Bio
+==========
+Simply and short description of the module's purpose,
+in this case the intial characterization of a person.
 
 @see <link_to_relevant_page>
 """
 
-# Author: name <email>
-# License: license model
+# Author: Sven Eschlbeck <sven.eschlbeck@t-online.de>
+# License: MIT
+
+class Person:
+  """
+  Initializes a person with name
+  and age.
+  """
+  def __init__(self, name, age):
+    """
+    Receives name and age as
+    parameters.
+    """
+    self.name = name
+    self.age = age
+    
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
