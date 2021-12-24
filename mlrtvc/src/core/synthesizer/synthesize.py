9e9b10f01aaa82bf9cb3b0fc4bf3d854f204da0e
raccoonML <95999607+raccoonML@users.noreply.github.com>
2021-12-24 07:12:12 -0800

    Make vocoder preproc code more elegant.

--- mlrtvc/src/core/synthesizer/synthesize.py
+++ mlrtvc/src/core/synthesizer/synthesize.py
@@ -12,9 +12,8 @@ import platform
 
 def run_synthesis(in_dir, out_dir, model_dir, hparams):
     # This generates ground truth-aligned mels for vocoder training
-    Path(out_dir).mkdir(exist_ok=True)
     synth_dir = Path(out_dir).joinpath("mels_gta")
-    synth_dir.mkdir(exist_ok=True)
+    synth_dir.mkdir(parents=True, exist_ok=True)
     print(hparams_debug_string())
 
     # Check for GPU
