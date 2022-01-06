38cff169b9e4e7ef42ac90317ad1df6831597e00
raccoonML <95999607+raccoonML@users.noreply.github.com>
2022-01-06 01:14:16 -0800

    Linting fix for audio library.

--- mlrtvc/src/core/synthesizer/audio.py
+++ mlrtvc/src/core/synthesizer/audio.py
@@ -28,14 +28,17 @@ from scipy import signal
 
 _mel_basis = None
 
+
 def load_wav(path, sr):
     # Loads an audio file and returns the waveform data.
     wav, _ = librosa.load(str(path), sr=sr)
     return wav
 
+
 def save_wav(wav, path, sr):
     # Saves waveform data to audio file.
     sf.write(path, wav, sr)
+
 
 def melspectrogram(wav, hparams):
     # Converts a waveform to a mel-scale spectrogram.
@@ -46,10 +49,12 @@ def melspectrogram(wav, hparams):
         wav = preemphasis(wav, hparams.preemphasis)
 
     # Short-time Fourier Transform (STFT)
-    D = librosa.stft(y=wav,
-                     n_fft=hparams.n_fft,
-                     hop_length=hparams.hop_size,
-                     win_length=hparams.win_size)
+    D = librosa.stft(
+        y=wav,
+        n_fft=hparams.n_fft,
+        hop_length=hparams.hop_size,
+        win_length=hparams.win_size,
+    )
 
     # Convert complex-valued output of STFT to absolute value (real)
     S = np.abs(D)
@@ -68,10 +73,11 @@ def melspectrogram(wav, hparams):
 
     return S.astype(np.float32)
 
+
 def inv_mel_spectrogram(S, hparams):
     # Converts a mel spectrogram to waveform using Griffin-Lim
     # Input shape = (num_mels, frames)
-    
+
     # Denormalize
     S = np.exp(S)
 
@@ -98,11 +104,13 @@ def inv_mel_spectrogram(S, hparams):
 
     return wav
 
+
 def preemphasis(wav, k, preemphasize=True):
     # Amplifies high frequency content in a waveform.
     if preemphasize:
         wav = signal.lfilter([1, -k], [1], wav)
     return wav
+
 
 def inv_preemphasis(wav, k, inv_preemphasize=True):
     # Inverts the preemphasis filter.
@@ -110,16 +118,37 @@ def inv_preemphasis(wav, k, inv_preemphasize=True):
         wav = signal.lfilter([1], [1, -k], wav)
     return wav
 
+
 def _build_mel_basis(hparams):
-    return librosa.filters.mel(hparams.sample_rate, hparams.n_fft, n_mels=hparams.num_mels,
-                               fmin=hparams.fmin, fmax=hparams.fmax)
+    return librosa.filters.mel(
+        hparams.sample_rate,
+        hparams.n_fft,
+        n_mels=hparams.num_mels,
+        fmin=hparams.fmin,
+        fmax=hparams.fmax,
+    )
+
 
 def _griffin_lim(S, hparams):
     angles = np.exp(2j * np.pi * np.random.rand(*S.shape))
     S = np.abs(S).astype(np.complex)
-    wav = librosa.istft(S * angles, hop_length=hparams.hop_size, win_length=hparams.win_size)
+    wav = librosa.istft(
+        S * angles, hop_length=hparams.hop_size, win_length=hparams.win_size
+    )
     for i in range(hparams.griffin_lim_iters):
-        angles = np.exp(1j * np.angle(librosa.stft(wav, n_fft=hparams.n_fft, hop_length=hparams.hop_size, win_length=hparams.win_size)))
-        wav = librosa.istft(S * angles, hop_length=hparams.hop_size, win_length=hparams.win_size)
+        angles = np.exp(
+            1j
+            * np.angle(
+                librosa.stft(
+                    wav,
+                    n_fft=hparams.n_fft,
+                    hop_length=hparams.hop_size,
+                    win_length=hparams.win_size,
+                )
+            )
+        )
+        wav = librosa.istft(
+            S * angles, hop_length=hparams.hop_size, win_length=hparams.win_size
+        )
 
     return wav
